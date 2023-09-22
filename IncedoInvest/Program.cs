755807a3cdb5818ba5aab350663eb09b9e1279c4
Devansh Jain <devanshdjboss@gmail.com>
2023-09-22 13:59:08 +0530

    New commit

--- IncedoInvest/Program.cs
+++ IncedoInvest/Program.cs
@@ -1,15 +1,23 @@
 using FluentAssertions.Common;
+using IncedoInvest.Application.Handlers.CommandHandlers;
 using IncedoInvest.Domain.Interfaces;
 using IncedoInvest.Infrastructure.DBContext;
 using IncedoInvest.Infrastructure.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+//builder.Services.AddMediatR(typeof(Program));
+
+
+builder.Services.AddMediatR(typeof(LoginAdvisorHandler).GetTypeInfo().Assembly);
+//builder.Services.AddMediatR(typeof(RegisterAdvisorHandler).GetTypeInfo().Assembly);
+
 builder.Services.AddDbContext<AdvisorDbContextClass>(
     options =>
     {
@@ -17,8 +25,9 @@ builder.Services.AddDbContext<AdvisorDbContextClass>(
             builder.Configuration.GetConnectionString("DefaultConnection"),
             x => x.MigrationsAssembly("IncedoInvest.Infrastructure"));
     });
-builder.Services.AddScoped<IAdvisorRepository, AdvisorRepository>();
 
+builder.Services.AddScoped<IAdvisorRepository, AdvisorRepository>();
+builder.Services.AddMediatR(typeof(Program));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
