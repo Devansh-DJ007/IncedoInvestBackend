e7c9ceff31309f423d02b919d531ac4985cb5acb
Dhruv Rai <dhruv.rai@incedoinc.com>
2023-10-06 14:34:37 +0530

    added investment strategy

---
+++ Application/InvestmentStrategyApp/Commands/CreateInvestmentStrategyCommand.cs
@@ -0,0 +1,43 @@
+﻿using IncedoInvest.Application.Services;
+using IncedoInvest.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace IncedoInvest.Application.InvestmentStrategyApp.Command
+{
+   public class CreateInvestmentStrategyCommand
+    {
+        [Required]
+        public int InvestmentStrategyId { get; set; }
+
+        [Required(ErrorMessage = "Strategy Name is required")]
+        [StringLength(200, ErrorMessage = "Strategy Name cannot exceed 200 characters")]
+        public string InvestmentStrategyName { get; set; }
+
+        [Required(ErrorMessage = "InvestmentTypeId is required")]
+        public int InvestmentTypeId { get; set; }
+
+        [Required(ErrorMessage = "10Y Return is required")]
+        public double Return10Y { get; set; }
+
+        [Required(ErrorMessage = "10Y Risk is required")]
+        public double Risk10Y { get; set; }
+
+        [Required(ErrorMessage = "1Y Return is required")]
+        public double Return1Y { get; set; }
+
+        [Required(ErrorMessage = "Deleted Flag is required")]
+        public bool DeletedFlag { get; set; }
+
+        [ForeignKey("InvestmentTypeId")]
+        public InvestmentType InvestmentType { get; set; }
+    }
+}
---
+++ Application/InvestmentStrategyApp/Commands/DeleteInvestmentStrategyCommand.cs
@@ -0,0 +1,17 @@
+﻿using IncedoInvest.Application.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IncedoInvest.Application.InvestmentStrategyApp.Command
+{
+    public class DeleteInvestmentStrategyCommand
+    {
+        public int InvestmentId { get; set; } 
+    }
+}
